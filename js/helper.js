995e6a19235acc2e02e19ad8ed990f6d86d1a284
Bob <bob@theparamountgroup.us>
2015-08-07 21:08:50 -0400

    project upload

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="white-text-role">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -24,10 +24,9 @@ var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
-
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLwelcomeMsg = '<h3 id="welcome-msg-h3"><span class="welcome-message">%data%</span></h3>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-list"></ul>';
+var HTMLskills = '<li class="flex-skills"><span class="white-text-skills">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -63,7 +62,7 @@ var googleMap = '<div id="map"></div>';
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
-  $('button').click(function() {
+$('button').click(function() {
     var iName = inName() || function(){};
     $('#name').html(iName);
   });
@@ -110,7 +109,7 @@ function initializeMap() {
   };
 
 
-  map = new google.maps.Map(document.querySelector('#map-div'), mapOptions);
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
@@ -169,7 +168,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infowindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,216 +1,287 @@
 
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+Udactiy Front End Developer course Project-2 Onoline Resume. */
 
- var bio = {
+
+var bio = {
  	"name" : "Bob Ingram",
- 	"role" : "President",
+ 	"role" : "Web Developer",
  	"contacts" : {
  		"mobile" : "321-345-8288",
  		"email" : "bob@theparamountgroup.us",
- 	    "github" : "paramountgroup",
- 	    "twitter" : "@paramountmedia",
- 	    "location" : "Satellite Beach",
+ 		"github" : "paramountgroup",
+ 		"twitter" : "@paramountmedia",
+ 		"location" : "Satellite Beach, FL",
  	},
- 	"welcomeMessage" : "Welcome to my Resume site",
- 	"skills" : ["Front-End Web Developer","Entrepreneur","Nuclear Engineer","Adwords Certified Professional"],
+ 	"welcomeMessage" : "President - The Paramount Group.us inc. We help our customers Extend Their Lead Online",
+ 	"skills": ["Website Designer", "Nuclear Engineer", "Entreprenuer", "Adwords Certified Consultant"],
  	"bioPic" : "images/bob_ingram_avatar.jpg",
+ 	"photos" : ["images/first-office.jpg","images/keylargo.jpg", "images/mugrace.jpg", "images/lasers.jpg", "images/first-office.jpg", "images/satellite-beach.jpg"],
 };
-var name = "Bob Ingram";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
 
-  var formattedMob = HTMLmobile.replace("%data%", bio.contacts.mobile);
-   $("#topContacts").prepend(formattedMob);
+bio.display = function() {
 
-        var formattedLoc = HTMLlocation.replace("%data%", bio.contacts.location);
-        $("#topContacts").prepend(formattedLoc);
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
 
-        var formattedTwit = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-        $("#topContacts").prepend(formattedTwit);
+	var formattedMob = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").prepend(formattedMob);
+	$("#footerContacts").prepend(formattedMob);
 
-        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-        $("#topContacts").prepend(formattedGithub);
+	var formattedLoc = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").prepend(formattedLoc);
+	$("#footerContacts").prepend(formattedLoc);
 
-        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-        $("#topContacts").prepend(formattedEmail);
+	var formattedTwit = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	$("#topContacts").prepend(formattedTwit);
+	$("#footerContacts").prepend(formattedTwit);
 
-         var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-        $("#header").append(formattedPic);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").prepend(formattedGithub);
+	$("#footerContacts").prepend(formattedGithub);
 
-if (bio.skills.length > 0){
-	$("#header").append(HTMLskillsStart);
-	var formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%",bio.skills[1]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%",bio.skills[2]);
-	$("#skills").append(formattedSkill);
-	formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
-	$("#skills").append(formattedSkill);
-    };
-/*
-        var formattedWelMsg = HTMLWelcomeMsg.replace("%data%", bio.WelcomeMsg);
-        $("#topContacts").prepend(formattedMob);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").prepend(formattedEmail);
+	$("#footerContacts").prepend(formattedEmail);
 
-        $("#footerContacts").prepend(formattedLoc);
+	var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+	$("#header").append(formattedPic);
+	var HTMLearthPic = '<img src="images/earth-optimized.gif" class="earthpic" id="earthId"/>';
+	$("#header").append(HTMLearthPic);
 
-        $("#footerContacts").prepend(formattedTwit);
+/* Additonal Functionality added here to show photos that give a better picture of Bob Ingram's favorite activites */
 
-        $("#footerContacts").prepend(formattedGithub);
+	$("#earthId").hover(function() {
+		var photoi = Math.floor((Math.random() * 5) + 1);
+		  $(this).attr('src', bio.photos[photoi]);
+		},
+		function() {
+			  $(this).attr('src', 'images/earth-optimized.gif');
+		});
+/* End additional functionality*/
 
-        $("#footerContacts").prepend(formattedEmail);
+	var formattedWelMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedWelMsg);
 
-        $("#footerContacts").prepend(formattedMob);
+	if(bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+
+	    for(i in bio.skills) {
+			$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+		}
+	}
+};
+
+bio.display();
 
 
-        var formattedListStart = HTMLskillsStart.replace("box", "list");
-        $("#header").append(formattedWelMsg);
-
-*/
-
-    var work = {
- 	"jobs" : [
-    {
-	"jobPosition" : "Internet Marketing Consultant",
+var work = {
+	"jobs" : [
+	{
 	"employer" : "The Paramount Group.us",
-	"yearsworked" : 2,
-	"city" : "Satellite Beach FL, US",
+	"title" : "Internet Marketing Consultant",
+	"location" : "Satellite Beach FL",
 	"dates" : "April 2013 - present",
 	"description" : "Certified Google Adwords and Analytics consultant managing client accounts to achieve internet advertising goals. Operations manager for the company to include finance, compliance and strategy",
 	},
 	{
-	"jobPosition" : "Printer",
+	"title" : "Printer",
 	"employer" : "Paramount Printing",
-	"yearsworked" : 20,
-	"city" : "Brunswick, GA US",
+	"location" : "Brunswick, GA",
 	"dates" : "1994 - April 2013",
 	"description" : "Printing production management, Direct mail and marketing, Management and financial oversight",
+	},
+	{
+	"title" : "General Contractor",
+	"employer" : "Classic Columns Renovations, DBA Eco-built, LLC",
+	"location" : "Saint Simons Island, GA",
+	"dates" : "2002 - 2009",
+	"description" : "Building Renovation, Energy Efficiency Improvements, Elastomeric Roofing, Solar Hot Water Installer & Dealer"
+	},
+	{
+	"title" : "Naval Officer - USS Will Rogers",
+	"employer" : "United States Navy",
+	"location" : "Holy Loch, Scottland",
+	"dates" : "1987 - 1994",
+	"description" : "Qualified Navy Nuclear Engineer, Strategic Weapons Officer Insstructor, Sonar Officer, Reactor Control Assistant",
 	}
-    ]
-};
-for (job in work.jobs) {
-    // create new dive for work experience
-	$("#workExperience").append(HTMLworkStart);
-    // concat employer and title
-	var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].jobPosition);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
-	$(".work-entry:last").append(formattedEmployerTitle);
-
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	$(".work-entry:last").append(formattedDates);
-
-	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	$(".work-entry:last").append(formattedDescription);
+	]
 };
 
-  var projects = {
- 	"project" : [
- 	   {
- 	   	"title" : "Udacity Project 1 Website",
- 	    "date" : "July 2015",
- 	    "description" : "First Udacity Project coding in HTML and CSS",
- 	    "images" : ["images/fry.jpg", "images/fry.jpg"],
- 	   },
- 	   {
- 	   	"title" : "Udacity Project 2 Resume Website",
- 	    "date" : "August 2015",
- 	    "description" : "First Udacity Project coding in Java Script and JSON",
- 	    "images" : ["images/fry.jpg", "images/fry.jpg"],
- 	   },
+work.display = function() {
 
-     ]
-     };
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+	// concat employer and title
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		$(".work-entry:last").append(formattedLocation);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+
+
+
+	}
+};
+
+work.display();
+
+var projects = {
+	"project" : [
+	{
+	"title" : "Udacity Project 1 Portfolio Website",
+	"date" : "July 2015",
+	"description" : "First Udacity Project coding in HTML and CSS",
+	"images" : ["images/portfolio-project-Website-350x291.jpg"],
+	},
+	{
+	"title" : "Udacity Project 2 Online Resume Website",
+	"date" : "August 2015",
+	"description" : "First Udacity Project coding in Java Script and JSON",
+	"images" : ["images/project-2-online-resume-350x391.jpg"],
+	},
+	]
+};
 
 projects.display = function() {
 
 	for (proj in projects.project) {
- 		$("#projects").append(HTMLprojectStart);
+		$("#projects").append(HTMLprojectStart);
 
- 		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[proj].title);
- 		$(".project-entry:last").append(formattedTitle);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[proj].title);
+		$(".project-entry:last").append(formattedTitle);
 
- 		var formattedDates = HTMLprojectDates.replace("%data%", projects.project[proj].date);
- 		$(".project-entry:last").append(formattedDates);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.project[proj].date);
+		$(".project-entry:last").append(formattedDates);
 
- 		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[proj].description);
- 		$(".project-entry:last").append(formattedDescription);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[proj].description);
+		$(".project-entry:last").append(formattedDescription);
 
-        if (projects.project[proj].images.length > 0) {
+		if (projects.project[proj].images.length > 0) {
 
-        	for (img in projects.project[proj].images) {
+			for (img in projects.project[proj].images) {
 
-        		var formattedImage = HTMLprojectImage.replace("%data%", projects.project[proj].images[img]);
-
-        		$(".project-entry:last").append(formattedImage);
-        	}
-        }
-    }
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.project[proj].images[img]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
 };
 
 projects.display();
 
- var education = {
- 	"schools" : [
- 	  {
- 		"name" : "University of Florida",
- 		"city" : "Gainesville, FL, US",
- 		"degree" : "Bachelor of Science",
- 		"major" : ["Computer Science Engineer"],
- 		"dates" : 1987,
- 		"url" : "http://www.theparamountgroup.us"
- 	  },
-      {
-        "name" : "Brenau University",
-        "city" : "Gainesville, GA, US",
-        "degree" : "Masters of Business Administration",
-        "major" : ["Business Administration"],
-        "dates" : 1994,
-        "url" : "http//www.theparamountgroup.us",
-      }
-    ],
-    "onlineCourses": [
-      {
-         "title" : "Udacity Front End Web Developer",
-         "school" : "Udacity",
-         "dates" : 2015,
-         "url" : "http://www.udacity.com/"
-      }
-    ]
- };
+
+var education = {
+	"schools" : [
+		{
+		"name" : "University of Florida",
+		"location" : "Gainesville, FL, US",
+		"degree" : "Bachelor of Science",
+		"majors" : ["Computer Science Engineer"],
+		"dates" : 1987,
+		"url" : "https://www.eng.ufl.edu/"
+		},
+		{
+		"name" : "Brenau University",
+		"location" : "Gainesville, GA, US",
+		"degree" : "Masters of Business Administration",
+		"majors" : ["Business Administration"],
+		"dates" : 1994,
+		"url" : "http://www.brenau.edu/",
+		}
+	],
+
+	"onlineCourses" : [
+		{
+		"title" : "Udacity Front End Web Developer",
+		"school" : "Udacity",
+		"date" : 2015,
+		"url" : "http://www.udacity.com/"
+		}
+	]
+};
+
+education.display = function () {
+	if(education.schools.length > 0 || education.onlineCourses.length > 0) {
+
+		for(ed in education.schools) {
+				$("#education").append(HTMLschoolStart);
+
+				var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[ed].name).replace("#", education.schools[ed].url);
+				var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[ed].degree);
+				var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[ed].dates);
+				var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[ed].location);
+				var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[ed].majors);
 
 
+				$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+				$(".education-entry:last").append(formattedSchoolDates);
+				$(".education-entry:last").append(formattedSchoolLocation);
+				$(".education-entry:last").append(formattedSchoolMajor);
+
+		};
+
+		if(education.onlineCourses.length > 0) {
+				$("#education").append(HTMLonlineClasses);
+
+				for(ol in education.onlineCourses) {
+					$("#education").append(HTMLschoolStart);
+
+					var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[ol].title).replace("#", education.onlineCourses[ol].url);
+					var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[ol].school);
+					var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[ol].date);
+					var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[ol].url).replace("#", education.onlineCourses[ol].url);
+
+					$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+					$(".education-entry:last").append(formattedOnlineDates);
+					$(".education-entry:last").append(formattedOnlineURL);
+				}
+
+
+		}
+	}
+};
+
+education.display();
 
 
 $(document).click(function(loc) {
-  var x = loc.pageX;
-  var y = loc.pageY;
 
-  logClicks(x,y);
+	var x = loc.pageX;
+	var y = loc.pageY;
 
+	logClicks(x,y);
 });
 
 function inName() {
-    var name = bio.name.split(" ");
-    name[1] = name[1].toUpperCase();
-    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-    bio.name = name[0] + " " + name[1];
-    return bio.name;
+	var name = bio.name.split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+	bio.name = name[0] + " " + name[1];
+	return bio.name;
 }
 function usName() {
-    var name = bio.name.split(" ");
-    name[1] = name[1].slice(0, 1).toUpperCase() + name[1].slice(1).toLowerCase();
-    name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-    bio.name = name[0] +" "+ name[1];
-    return bio.name;
+	var name = bio.name.split(" ");
+	name[1] = name[1].slice(0, 1).toUpperCase() + name[1].slice(1).toLowerCase();
+	name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+	bio.name = name[0] +" "+ name[1];
+	return bio.name;
 };
 $("#main").append(internationalizeButton);
 
 $("#map-div").append(googleMap);
+
+google.maps.event.addListener(marker, 'click', function() {
+// your code goes here
+});
